fd0322d6bd13646b0ba4c540136e1eb1a452a6d4
xiangyu <xiangyuzhang365@gmail.com>
2015-10-01 23:22:36 -0400

    feat: slightly change to make more plot

--- XOR_Cam/XOR_loop.py
+++ XOR_Cam/XOR_loop.py
@@ -1,9 +1,43 @@
 __author__ = 'xiangyuzhang'
 from subprocess import call
 
-camlist = ["c499-mux4-1.v", "c499-mux4-11.v", "c499-mux4-21.v", "c499-mux4-31.v", "c499-mux4-41.v", "c499-mux4-51.v",
-           "c499-mux4-61.v", "c880-mux4-1.v", "c880-mux4-11.v","c880-mux4-21.v", "c880-mux4-31.v","c880-mux4-41.v",
-           "c880-mux4-51.v", "c880-mux4-61.v"]
+camlist = [
+    "c7552-mux4-1.v",
+    "c7552-mux4-11.v",
+    "c7552-mux4-21.v",
+    "c7552-mux4-31.v",
+    "c7552-mux4-41.v",
+    "c7552-mux4-51.v",
+    "c7552-mux4-61.v",
+    "c7552-mux4-71.v",
+    "c7552-mux4-81.v",
+    "c7552-mux4-91.v",
+    "c7552-mux4-101.v",
+    "c7552-mux4-111.v",
+    "c7552-mux4-121.v",
+    "c7552-mux4-131.v",
+    "c7552-mux4-141.v",
+    "c7552-mux4-151.v",
+    "c7552-mux4-161.v",
+    "c7552-mux4-171.v",
+    "c7552-mux4-181.v",
+    "c7552-mux4-191.v",
+    "c7552-mux4-201.v"
+    ]
 for i in camlist:
     camd = "python XOR_Cam.py " + i
     call(camd, shell = True)
+
+for j in range(1,202,10):
+        camd = "python XOR_Cam.py c5315-mux4-" + str(j) + ".v"
+        call(camd, shell = True)
+
+for k in range(1,202,10):
+        camd = "python XOR_Cam.py c1908-mux4-" + str(k) + ".v"
+        call(camd, shell = True)
+
+for e in range(1,202,10):
+        camd = "python XOR_Cam.py c1355-mux4-" + str(e) + ".v"
+        call(camd, shell = True)
+
+
