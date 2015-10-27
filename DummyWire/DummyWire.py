12e13d50b6d3be240be50c92255c60c258be8f79
xiangyu <xiangyuzhang365@gmail.com>
2015-10-26 23:46:45 -0400

    feat: 终于把MUX2model搞好了

--- DummyWire/DummyWire.py
+++ DummyWire/DummyWire.py
@@ -4,8 +4,32 @@ import sys
 import os
 from wire_CircuitScanner import CircuitScanner
 
+def abcmap_MUX_OBF_netlist(pi1, pi2, pi3, pi4, output, seed, programbit):
+    D_bit1 = 'D_' + str(programbit)
+    D_bit2 = 'D_' + str(programbit + 1)
+    D_bit1_not = 'D_' + str(programbit) + "_NOT"
+    D_bit2_not = 'D_' + str(programbit + 1) + "_NOT"
+    new_netlist = []
+    new_netlist.append( "inv1 gate( .a(" + D_bit1 + "),.O(" +  D_bit1_not + ") );\n")
+    new_netlist.append( "inv1 gate( .a(" + D_bit2 + "),.O(" +  D_bit2_not + ") );\n")
+    new_netlist.append("and2 gate( .a(" + pi1 + "), .b(" + D_bit1_not + "), .O(" + "ED_" + str(seed) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + pi2 + "), .b(" + D_bit1 + "), .O(" + "ED_" + str(seed + 1) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + pi3 + "), .b(" + D_bit1_not + "), .O(" + "ED_" + str(seed + 2) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + pi4 + "), .b(" + D_bit1 + "), .O(" + "ED_" + str(seed + 3) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + "ED_" + str(seed) + "), .b(" + D_bit2_not + "), .O(" + "ED_" + str(seed + 4) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + "ED_" + str(seed + 1) + "), .b(" + D_bit2 + "), .O(" + "ED_" + str(seed + 5) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + "ED_" + str(seed + 2) + "), .b(" + D_bit2_not + "), .O(" + "ED_" + str(seed + 6) + ") );\n")
+    new_netlist.append("and2 gate( .a(" + "ED_" + str(seed + 3) + "), .b(" + D_bit2 + "), .O(" + "ED_" + str(seed + 7) + ") );\n")
+    new_netlist.append("or2  gate( .a(" +  "ED_" + str(seed + 4) + "), .b(" + "ED_" + str(seed + 5) +"), .O(" + "ED_" + str(seed + 8) +") );\n")
+    new_netlist.append("or2  gate( .a(" +  "ED_" + str(seed + 6) + "), .b(" + "ED_" + str(seed + 8) +"), .O(" + "ED_" + str(seed + 9) +") );\n")
+    new_netlist.append("or2  gate( .a(" +  "ED_" + str(seed + 9) + "), .b(" + "ED_" + str(seed + 7) +"), .O(" + str(output) +") );\n")
+    return new_netlist
+
+
+print abcmap_MUX_OBF_netlist("N1", "N2", "N3", "N4", "Output", 0, 1)
 
 pair_list = []  # used to store the random selected gate pair
+
 # parser
 parser = argparse.ArgumentParser(usage='python DummyWire.py [-h]  <circuit.v> [number]]', description='This program will camouflage <circuit.v> with dummy wire',)
 parser.add_argument('<circuit.v>', help='input circuit to be camouflaged')
