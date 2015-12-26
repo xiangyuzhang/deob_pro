1e1e9682d7f3196341b7a216d9b7669d5aacc820
xiangyu <xiangyuzhang365@gmail.com>
2015-12-26 15:47:03 -0500

    完成了分级，现在需要将信息返回为一个list

--- Levelization/Levelizer.py
+++ Levelization/Levelizer.py
@@ -2,75 +2,26 @@ __author__ = 'xiangyuzhang'
 from networkx import nx
 import re
 
+def add_edge(circuit, input, output):   # add edge and also initialize type = else and level = -1
+    if input not in circuit.nodes():
+        circuit.add_node(input, type = "else", level = -1)
+    if output not in circuit.nodes():
+        circuit.add_node(output,type = "else", level = -1)
+    circuit.add_edge(input,output)
 
-class Graph_class:
-    def __init__(self, nodelist=[], edgelist=[], node_name_list=[], vertexes=0):
-        self.nodelist = nodelist
-        self.edgelist = edgelist
-        self.node_name_list = node_name_list
-        self.nodes = vertexes
-
-
-    def Add_edge(self, front_name, back_name):
-        if front_name not in self.node_name_list:
-            input = Wire_class(front_name)
-            self.nodelist.append(input)
-            self.node_name_list.append(front_name)
-            self.nodes += 1
-        if back_name not in self.node_name_list:
-            output = Wire_class(back_name)
-            self.nodelist.append(output)
-            self.node_name_list.append(back_name)
-            self.nodes += 1
-        for i in self.nodelist:
-            if i.name == front_name:
-                i.back.append(back_name)
-                break
-        for j in self.nodelist:
-            if j.name == back_name:
-                j.front.append(front_name)
-                break
-        print ''
-
-
-    def print_circuit(self, type):
-        if type == "node_forward":
-            for i in self.nodelist:
-                print "Node: ", i.name, ":\n"
-                for j in i.back:
-                    print j, " "
-                print "\n"
-
-class Gate_class:  # gate actually is just an edge to connect wires
-    gate_count = 0
-
-    def __init__(self, input_list=[], output_name="empty"):
-        self.input_list = input_list
-        self.output_name = output_name
-        gate_count = + 1
-
-
-class Wire_class:
-    wire_count = 0
-
-    def __init__(self, name, front=[], back=[]):
-        self.name = name
-        self.back = back
-        self.front = front
-        Wire_class.wire_count += 1
-
-
-def gate_scaner(gate_info_string, circuit):
+def gate_scaner(gate_info_string, circuit):         # used to process gate info, connect input info and output info
     gate_info = parser(gate_info_string)
     input_list = gate_info[0:-1]  # input and output here is just string
     output = gate_info[-1]
     for input in input_list:
-        circuit.Add_edge(input, output)
+        add_edge(circuit, input, output)
+
+def checker(circuit):
+    print "The following is nodes info:", circuit.nodes(data = True), "\n"
+    print "The following is edges info:", circuit.edges(data = True), "\n"
 
 
-
-
-def parser(gate_info_string):
+def parser(gate_info_string):   # parse line to find corresponding netname
     return_list = []
     temp_list = re.findall(r'(\(\w*\))', gate_info_string)
     for i in temp_list:
@@ -78,13 +29,82 @@ def parser(gate_info_string):
             return_list.append(i.strip('(').strip(')'))
     return return_list
 
-circuit = Graph_class()
-with open("c432-abcmap-fmt-WIRE-2-original.v", "r") as infile:
+def input_initializer(input_info, circuit):     # process "input" info, and initlize type = input and make level = 1
+    input_info = input_info.strip("input").strip(" ").strip(";")
+    input_info = input_info.split(",")
+    for input in input_info:
+        if input not in circuit.nodes():
+            circuit.add_node(input, type = "input", level = 1)
+#   print circuit.nodes(data = True)
+
+def all_its_precessor_is_not(node,circuit):
+    level = nx.get_node_attributes(circuit,"level")
+    # print node, "has predecessors: "
+    for pre in circuit.predecessors(node):
+        # print pre, " with level = ", level[pre]
+        if level[pre] == -1:
+#             print "node: ", node, " has predecessor: ", pre, " is not initialized"
+            return False
+    return True
+
+def calculate_its_level(node,circuit,queue):
+    # level = nx.get_node_attributes(circuit, "level")
+    for pre in circuit.predecessors(node):
+        if circuit.node[pre]["level"] >= circuit.node[node]["level"]:
+#           queue[node][1]["level"] = level[pre]
+#           circuit[node][1]["level"] = level[pre]
+#            nx.set_node_attributes(circuit,"level",{node:level[pre]})
+            circuit.node[node]["level"] = circuit.node[pre]["level"]
+    circuit.node[node]["level"] += 1
+#     print "node:", node, " has level = ", circuit.node[node]["level"]
+
+
+def levelizer(circuit):
+    # initialize queue
+    queue = []
+
+    # inqueue input's successor
+    for node in circuit.nodes(data = True):
+        if node[1]["type"] == "input":
+            for i in circuit.successors(node[0]):
+                if i not in queue:
+                    queue.append(i)
+    # print queue
+    # dequeue, if its precessor is empty or all its precessor is not -1, then calculate its level, inqueue otherwise
+
+    while(len(queue) != 0):
+        temp = queue.pop(0)
+#         print "pop", temp
+        if all_its_precessor_is_not(temp, circuit) == True:
+            calculate_its_level(temp, circuit,queue)
+#             print temp, " has successor: ", circuit.successors(temp)
+            for next in circuit.successors(temp):
+                if next not in queue:
+                    queue.append(next)
+#                     print "enqueue1: ", next
+        else:
+            if temp not in queue:
+                queue.append(temp)
+#                 print "inqueue2: ", temp
+#         print "\t---------------------------------------------"
+
+
+
+
+circuit = nx.DiGraph()
+with open("c17-abcmap-fmt.v", "r") as infile:
     Vline = infile.read().split(";")
 
 for line in Vline:
     line = line.strip("\n")
+    if "input" in line:
+        input_initializer(line, circuit)
     if line != '' and '/' not in 'module' not in line and 'input' not in line and 'output' not in line and 'wire' not in line and 'endmodule' not in line:
         gate_scaner(line,circuit)
 
-circuit.print_circuit("node_forward")
+# checker(circuit)
+levelizer(circuit)
+checker(circuit)
+
+
+
